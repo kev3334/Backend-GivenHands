ba62765049022d03ea7eccce195fabb7d8124f7d
Kevin <kev3334@gmail.com>
2025-03-06 11:57:45 -0500

    agregando cors (*)  a los controladores

--- src/main/java/com/no_country/GivenHands/controller/RegisterUserController.java
+++ src/main/java/com/no_country/GivenHands/controller/RegisterUserController.java
@@ -5,11 +5,9 @@ import com.no_country.GivenHands.service.RegisterUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/userregister")
 public class RegisterUserController {
